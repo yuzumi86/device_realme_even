e491ec07b122b71897e2408333a6182ae1b1f570
yuzumi <157438148+yuzumi86@users.noreply.github.com>
2024-06-06 19:22:44 +0700

    even: PixelOS bring up

--- aosp_even.mk
+++ aosp_even.mk
@@ -15,7 +15,7 @@
 #
 
 # Inherit some common Pixel-Experience stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
@@ -27,7 +27,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 $(call inherit-product, device/realme/even/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_even
+PRODUCT_NAME := aosp_even
 PRODUCT_DEVICE := even
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme C25
