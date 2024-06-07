b7e2e74d3ff8bdcb5c170f8a918480bffe59f89d
yuzumi <157438148+yuzumi86@users.noreply.github.com>
2024-06-08 04:32:54 +0700

    even: Lineage BringUp

--- lineage_even.mk
+++ lineage_even.mk
@@ -15,7 +15,7 @@
 #
 
 # Inherit some common Pixel-Experience stuff.
-$(call inherit-product, vendor/cipher/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
@@ -27,7 +27,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/realme/even/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := cipher_even
+PRODUCT_NAME := lineage_even
 PRODUCT_DEVICE := even
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme C25
@@ -37,10 +37,6 @@ TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
 TARGET_BOOT_ANIMATION_RES := 720
-
-# Cipher additions
-CIPHER_MAINTAINER := ismasrull
-CIPHER_GAPPS := true
 
 # Build info
 BUILD_FINGERPRINT := "google/coral/coral:12/SQ3A.220705.003.A1/8672226:user/release-keys"
