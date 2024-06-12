95897b0fc77c1d86298c2de5f14567a9113d7d07
yuzumi <157438148+yuzumi86@users.noreply.github.com>
2024-06-12 09:12:53 +0700

    even:

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -17,22 +17,6 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.mt6768.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.mt6768.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.mt6785
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/fstab.mt6768
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := perf_profile.sh
 LOCAL_SRC_FILES := bin/perf_profile.sh
 LOCAL_MODULE_CLASS := EXECUTABLES
