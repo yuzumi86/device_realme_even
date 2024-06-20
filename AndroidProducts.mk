1ea0a3f0c459bc36693ea3ffb798423b3d36cce3
yuzumi <157438148+yuzumi86@users.noreply.github.com>
2024-06-20 08:30:04 +0700

    even: thirteen

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/fluid_even.mk
+    $(LOCAL_DIR)/lineage_even.mk
 
 COMMON_LUNCH_CHOICES := \
-    fluid_even-eng \
-    fluid_even-userdebug \
-    fluid_even-user
+    lineage_even-eng \
+    lineage_even-userdebug \
+    lineage_even-user
