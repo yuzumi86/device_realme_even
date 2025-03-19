fdc6dbe5e695889840e28dc83a51f572aee272bb
yuzumi <157438148+yuzumi86@users.noreply.github.com>
2025-03-19 08:39:03 +0700

    even: proton

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,5 +2,5 @@ echo "Cloning Dependencies"
 rm -rf ./prebuilts/clang/host/linux-x86/mylittleclang
 rm -rf ./kernel/realme/even
 git clone --depth=1 https://github.com/Realme-C25-Series-Development/kernel_realme_even.git -b even-rmui2 ./kernel/realme/even
-git clone --depth=1 https://github.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-6573524.git -b 10.0 ./prebuilts/clang/host/linux-x86/mylittleclang
+git clone --depth=1 https://github.com/kdrag0n/proton-clang.git -b master ./prebuilts/clang/host/linux-x86/mylittleclang
 echo "Done!"
