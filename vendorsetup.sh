e5116ea4f29dd35397bb36b7bb4a20df9b105bec
yuzumi <157438148+yuzumi86@users.noreply.github.com>
2025-03-19 12:52:12 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 echo "Cloning Dependencies"
 rm -rf ./prebuilts/clang/host/linux-x86/mylittleclang
 rm -rf ./kernel/realme/even
-git clone --depth=1 https://github.com/Realme-C25-Series-Development/kernel_realme_even.git -b even-rmui2 ./kernel/realme/even
-git clone --depth=1 --recurse-submodules https://github.com/Ancient-Project/clang-11.git -b 11.0.0 ./prebuilts/clang/host/linux-x86/mylittleclang
+git clone --depth=1  --recurse-submodules https://github.com/liquidprjkt/liquid_kernel_realme_even.git -b OSS-RUI2 ./kernel/realme/even
+git clone --depth=1 --recurse-submodules https://gitlab.com/clangsantoni/zyc_clang.git -b 14 ./prebuilts/clang/host/linux-x86/mylittleclang
 echo "Done!"
